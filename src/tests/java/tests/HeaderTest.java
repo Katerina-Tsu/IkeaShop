8c41a2c2b1876b12a1ccee37ab1f5a2f19b52e19
Kate White <Shmatok2020>
2023-05-14 00:17:14 +0300

    Add to git Header test.Green.

---
+++ src/tests/java/tests/HeaderTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class HeaderTest extends BaseTest {
+
+    @Test(groups = "Buttons frame", description = "Sign in to my account")
+    public void signInTest() {
+        mainProductsPage.openMainPage();
+        mainProductsPage.clickHejLogInOrSignUpButton();
+
+    }
+}
